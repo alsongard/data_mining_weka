8ed1f64138d3deefc28c07d8b46299751cf1b6e2
alsongard <alsongard@gmail.com>
2024-06-09 04:52:05 -0400

    cleaning data, adding quotation marks, value_counts

--- assignment_projects/accident_data_analysis/README.md
+++ assignment_projects/accident_data_analysis/README.md
@@ -8,16 +8,16 @@ This folder will be used for analsing road accidents data from the year 2005 to 
 The accident.csv file will be checked for any missing values or inconsistencies in the data
 1. Exploratory Data Anaysis
 Extract relevant data features from the accident.csv file  which are:
-Features extracted: |
-| Location | [] |
-| Time | [x] |
-| Weather | [x] |
-| Road Conditions | [x] |
-| Vehicle Types| []
-| Injury Severity |  [x] |
+Features extracted: 
+|   |   |   |
+| -  |  - | -  |
+|   |   |   |
+|   |   |   |
 
 Other Features added are : Urban or Rural Road and Number_of_Vehicles
 On the Urban_or_Rural data the values 
 1 : represent Urban
 2 : represent Rural
 3 : represent Other which is removed
+
+|   |   |   |
--- assignment_projects/accident_data_analysis/accident.py
+++ assignment_projects/accident_data_analysis/accident.py
@@ -1,7 +1,7 @@
 # this file will be used to clean, extract relevant information and save it to a file that will be later used in weka.
 
 import pandas as pd
-
+import csv 
 #load data set
 data_df = pd.read_csv("../../data_sets/accidents_2005_to_2007.csv")
 print(data_df.head(10))
@@ -29,5 +29,28 @@ print(new_data_df["Urban_or_Rural_Area"].value_counts())
 print(new_data_df["Number_of_Vehicles"].value_counts())
 new_data_df = new_data_df[new_data_df.Urban_or_Rural_Area != 3]
 print(new_data_df.shape)
+
+#adding comma to the time column
+# new_data_df.Time = ['"' + str(x) + '"' for x in new_data_df.Time]
+column_name = "Time"
+column_name_2 = "Weather_Conditions"
+new_data_df[column_name] = new_data_df[column_name].apply(lambda x : f'"{x}"')
+# df[column_name] = df[column_name].apply(lambda x: f'"{x}"')
+# df[column_name] = df[column_name].apply(lambda x: '"' + str(x) + '"')
+# new_data_df = '"' + new_data_df[column_name] + '"'
+new_data_df[column_name_2] = new_data_df[column_name_2].apply(lambda x : f'"{x}"')
+print(new_data_df.head)
+
+#checking values of  weather condition s
+print(new_data_df["Road_Surface_Conditions"].value_counts())
+#create a dictionary for replacements
+road_conditions_replacements = {
+    "Wet/Damp" : "Wet",
+    "Frost/Ice" : "Frost"
+}
+new_data_df["Road_Surface_Conditions"] = new_data_df["Road_Surface_Conditions"].replace(road_conditions_replacements)
+print(new_data_df["Road_Surface_Conditions"].value_counts())
+
+print(new_data_df["Weather_Conditions"].value_counts())
 #save to a new csv file
-# new_data_df.to_csv("../../data_sets/accidents_2005_to_2007_weka_data.arff", index=False)
+# new_data_df.to_csv("../../data_sets/accidents_2005_to_2007_weka_data.csv",index=False, quoting=csv.QUOTE_NONE)
